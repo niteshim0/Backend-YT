f21c17dff44a605ebc998d3fee4962646bb21982
niteshim0 <niteshim111@gmail.com>
2024-01-26 20:57:00 +0530

    register and controller part-1

--- src/app.js
+++ src/app.js
@@ -4,23 +4,26 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
-//CORS_ORIGIN contanins from which URL you are accessing backend
 app.use(cors({
   origin:process.env.CORS_ORIGIN,
   credentials:true
 }))
 
-//limits for how much data one can send to backend
 app.use(express.json({limt:"16kb"}))
 
-//data from urls can be in various forms
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 
-//some data is stored on our server,for using that
 app.use(express.static("public"))
 
-//for use of cookies stored in our browser via server
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.route.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
 
 export {app}
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,9 @@
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next))
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next))
   .catch((err)=>next(err));
+}
+}
+
+
+export {asyncHandler};
