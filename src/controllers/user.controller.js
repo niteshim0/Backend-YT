2fcc7a7969ca27eba2741fbffda336c776ae01df
niteshim0 <niteshim111@gmail.com>
2024-02-02 00:20:28 +0530

    getUserChannelProfile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -406,6 +406,20 @@ const getUserChannelProfile = asyncHandler( async(req,res) => {
     ]
    )
 
+   if(!channel?.length){
+    throw new ApiError(404,"Channel does not exist")
+   }
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(
+      200,
+      channel[0],
+      "User channel fetched successfully"
+    )
+   )
+
 })
 
 export { registerUser , loginUser,logOutUser, refreshAccessToken , changeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage , getUserChannelProfile};
