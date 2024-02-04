dc60bac6ff854f788c6f5d4c6916ea0e708ebb49
niteshim0 <niteshim111@gmail.com>
2024-02-04 21:19:18 +0530

    watchHistory controller using subpipeline and user route upadation

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { logOutUser, loginUser, refreshAccessToken, registerUser ,changeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , getUserChannelProfile , getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,4 +25,14 @@ router.route("/login").post(loginUser)
 //for these endpoints user must me logged in
 router.route("/logout").post( verifyJWT , logOutUser )
 router.route("/refresh-token").post( refreshAccessToken )
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router;
